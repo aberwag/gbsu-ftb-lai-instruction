3c72962c9669b4c76e34290151b016eb7ba3e7a6
aberwag <ahmed.aberwag@committer.fr>
2022-02-21 20:09:37 +0100

    :rocket: implemennt feature + test

--- gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/services/GbsuFtbLaiService.kt
+++ gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/services/GbsuFtbLaiService.kt
@@ -5,8 +5,36 @@ import org.springframework.stereotype.Component
 @Component
 class GbsuFtbLaiService {
 
+    companion object {
+        val DIV_NUMBER_KEYS = mapOf(3 to "Gbsu", 5 to "Ftb")
+        val CONTAIN_NUMBER_KEYS = mapOf(3 to "Gbsu", 5 to "Ftb", 7 to "Lai")
+    }
+
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        val response = StringBuilder()
+
+        // Number divisible
+        DIV_NUMBER_KEYS.forEach{ (k, v) ->
+            if (inputNumber % k == 0) {
+                response.append(v)
+            }
+        }
+
+        // Number is present
+        val inputStr = inputNumber.toString()
+        for (c in inputStr) {
+            CONTAIN_NUMBER_KEYS.forEach { (k, v) ->
+                if (c.toString().toInt() == k) {
+                    response.append(v)
+                }
+            }
+        }
+
+        // NO MATCH ?
+        if (response.isBlank()) {
+            return inputStr
+        }
+        return response.toString()
     }
 
 }
--- gbsuftblai-back/src/test/kotlin/com/kata/gbsuftblai/GbsuFtbLaiApplicationTests.kt
+++ gbsuftblai-back/src/test/kotlin/com/kata/gbsuftblai/GbsuFtbLaiApplicationTests.kt
@@ -1,13 +1,34 @@
 package com.kata.gbsuftblai
 
+import com.kata.gbsuftblai.services.GbsuFtbLaiService
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
+
 @SpringBootTest
-class GbsuFtbLaiApplicationTests {
+class GbsuFtbLaiApplicationTests(@Autowired val gbsuFtbLaiService: GbsuFtbLaiService) {
 
     @Test
     fun contextLoads() {
     }
 
+    @ParameterizedTest(name = "convertNumber should return {1}")
+    @CsvSource(
+            "1,1",
+            "3,GbsuGbsu",
+            "5,FtbFtb",
+            "7,Lai",
+            "9,Gbsu",
+            "51,GbsuFtb",
+            "53,FtbGbsu",
+            "33,GbsuGbsuGbsu",
+            "27,GbsuLai",
+            "15,GbsuFtbFtb")
+    fun convertNumber(input: Int, expected: String) {
+        assertEquals(expected, gbsuFtbLaiService.convertNumber(input))
+    }
 }
